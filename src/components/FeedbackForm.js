a7a8973575786b33e911b083e31b05fec39264f1
david Karanja <davidkaranja@davids-MacBook-Pro.local>
2024-10-31 10:30:15 +0300

    Completed creating Context & Provider, Global with useContext and Moving functions to Context

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -3,12 +3,16 @@ import { useState } from "react";
 import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import FeedbackContext from "../context/FeedbackContext";
+import { useContext } from "react";
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
+  const { addFeedback } = useContext(FeedbackContext);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -31,7 +35,7 @@ function FeedbackForm({ handleAdd }) {
         text,
         rating,
       };
-      handleAdd(newFeedback);
+      addFeedback(newFeedback);
 
       setText("");
     }
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,15 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { FaTimes } from "react-icons/fa";
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
 
 import Card from "./shared/Card";
 
 // This is a style component
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
+      <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
